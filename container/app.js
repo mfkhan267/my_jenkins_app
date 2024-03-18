9aa48d6d60ac2b7ace382305e9672167a62988f0
FerozeKhan <ferozekhan267oa@gmail.com>
2024-03-18 20:06:26 +0530

    updated webpage text

--- container/app.js
+++ container/app.js
@@ -10,7 +10,7 @@ app.use(express.static('static'));
 app.use(morgan('combined'));
 
 var port = process.env.PORT || 8080;
-var message = process.env.MESSAGE || "WebAssembly is the future!";
+var message = process.env.MESSAGE || "This WebApp is running as Container on Azure Cloud!";
 
 app.get('/', function (req, res) {
     res.render('home', {
